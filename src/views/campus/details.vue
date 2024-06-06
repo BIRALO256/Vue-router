11266a0f3563d94e2712e5f338276c331167cf95
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-06-06 22:16:12 +0300

    learnt basics of vue route

--- src/views/campus/details.vue
+++ src/views/campus/details.vue
@@ -1,13 +1,36 @@
 <template>
-    <h1>Lectures details Page</h1>
+
+    <div v-if="lecture" >
+
+    <h1>{{ lecture.title }}</h1>
     <p>the lecture id is {{ id }}</p>
+    <p>this is the dtails {{ lectures.detail }}</p>
+
+    </div>
+
+    <div v-else>
+    
+        <p>Loading........</p>
+
+    </div>
+
+   
 </template>
 
 <script>
 export default {
     props:['id'],
     data(){
+        return{
+            lecture :null // the detail data about the project is null but then we fill it after fetching it with the provided idS
+        }
         // id:this.$route.params.id
+    },
+    mounted(){
+        fetch('https://localhost:3000/lectures'+ this.id)
+        .then(res=>res.json())
+        .then(data =>this.lecture =data)
+        .catch(err=> console.log(err.message))
     }
 }
 </script>
--- src/views/campus/lectures.vue
+++ src/views/campus/lectures.vue
@@ -1,15 +1,24 @@
+//we are using this div just for the else part since we can't loop through an empty in this page componet because we get the the data from the end point anyway
 <template>
-     <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-    <h1 class="text-3xl font-bold mb-6 text-gray-800">Available Lectures</h1>
-    <div v-for="campus in lectures" :key="campus.id" class="mb-4">
-      <router-link
-        :to="{ name: 'lectures', params: { id: campus.id } }"
-        class="block p-4 bg-gray-100 hover:bg-gray-200 rounded-lg shadow transition"
-      >
-        <h2 class="text-xl font-semibold text-blue-600">{{ campus.title }}</h2>
-      </router-link>
+
+    <div v-if="lectures.length"> 
+            <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+            <h1 class="text-3xl font-bold mb-6 text-gray-800">Available Lectures</h1>
+            <div v-for="campus in lectures" :key="campus.id" class="mb-4">
+            <router-link
+                :to="{ name: 'lectures', params: { id: campus.id } }"
+                class="block p-4 bg-gray-100 hover:bg-gray-200 rounded-lg shadow transition"
+            >
+                <h2 class="text-xl font-semibold text-blue-600 bg-black ">{{ campus.title }}</h2>
+            </router-link>
+            </div>
+        </div>
     </div>
-  </div>
+    
+    <div v-else>
+        <p>Still loadingS</p>
+    </div>
+   
 </template>
 
 <script>
@@ -22,6 +31,14 @@ export default {
                 {title: 'computer leture',id :3 ,details:'lorem12'},
             ]
         }
+    },
+    mounted(){
+        //fetch returns a promise meaning it takes some time to return the object of data , res methon also returns a promis meaning we use data to get the acutual data
+        fetch('https://localhost:3000/lectures')
+        .then(res=>res.json())
+        .then(data =>this.lectures =data)
+        .catch(err=> console.log(err.message))// we assign the data of to lectures array or any array we have to populate with the data i get
+        // we use catch to get the erro incase we don't get the expected data
     }
 }
 </script>
